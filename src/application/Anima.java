a6904de69da7aa1d4de309d09f95f292ecfc1512
Mateusz Hryciuk <mateusz.hryciuk@outlook.com>
2017-11-21 21:12:18 +0100

    ver 1

---
+++ src/application/Anima.java
@@ -0,0 +1,184 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.*;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.animation.PathTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.SequentialTransition;
+import javafx.event.EventHandler;
+import javafx.animation.KeyFrame;
+import javafx.event.ActionEvent;
+import javafx.animation.FillTransition;
+import javafx.animation.ParallelTransition;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import java.io.File;
+
+public class Anima extends Application{
+	
+	
+		@Override
+		public void start (Stage primaryStage){
+	
+			
+			Media introSound =  new Media (getClass().getClassLoader().getResource("xylophone.wav").toString());
+			MediaPlayer mediaPlayer = new MediaPlayer(introSound);
+			
+
+			
+			
+			
+			 Pane pane = new Pane();
+			 
+			 Line line1 = new Line();
+			 line1.setStartX(0);
+			 line1.startYProperty().bind(pane.heightProperty().divide(2));
+			 line1.setEndX(330);
+			 line1.endYProperty().bind(pane.heightProperty().divide(2));
+			 
+			 Line line2 = new Line();
+			 line2.setStartX(0);
+			 line2.startYProperty().bind(pane.heightProperty().divide(4));
+			 line2.setEndX(459);
+			 line2.endYProperty().bind(pane.heightProperty().divide(2));
+			
+			 Line line3 = new Line();
+			 line3.setStartX(800);
+			 line3.startYProperty().bind(pane.heightProperty().divide(2));
+			 line3.setEndX(514);
+			 line3.endYProperty().bind(pane.heightProperty().divide(2));
+			 
+			 Line lineA = new Line();
+			 lineA.setStartX(400);
+			 lineA.startYProperty().bind(pane.heightProperty().divide(2));
+			 lineA.setEndX(320);
+			 lineA.endYProperty().bind(pane.heightProperty().divide(2));
+			 
+			 Line lineB = new Line();
+			 lineB.setStartX(459);
+			 lineB.startYProperty().bind(pane.heightProperty().divide(2));
+			 lineB.setEndX(379);
+			 lineB.endYProperty().bind(pane.heightProperty().divide(2));
+			 
+			 Line lineC = new Line();
+			 lineC.setStartX(514);
+			 lineC.startYProperty().bind(pane.heightProperty().divide(2));
+			 lineC.setEndX(434);
+			 lineC.endYProperty().bind(pane.heightProperty().divide(2));
+			 			 			 			 
+						 
+			 Font font =  Font.font ("SansSerif",FontWeight.BOLD,80);
+			 
+			Text labelA = new Text("A");
+			labelA.setFont(font);
+			
+			Text labelB= new Text("B");
+			labelB.setFont(font);
+			
+			Text labelC = new Text("C");
+			labelC.setFont(font);
+			
+			
+			 
+			 PathTransition path1 = new PathTransition();
+			 path1.setDuration(Duration.millis(500));
+			 path1.setPath(line1);
+			 path1.setNode(labelA);
+			 
+			 FillTransition  fillA = new FillTransition ( Duration.millis(1),labelA,Color.web("#242424"),Color.web("#C00909"));
+			 
+			 PathTransition path2 = new PathTransition();
+			 path2.setDuration(Duration.millis(500));
+			 path2.setPath(line2);
+	
+			 
+			 RotateTransition rotate = new RotateTransition();
+			 rotate.setDuration(Duration.millis(500));
+			 rotate.setByAngle(720);
+	
+			 
+			 ParallelTransition parallel = new ParallelTransition (labelB,path2,rotate);
+			 
+			 FillTransition  fillB = new FillTransition ( Duration.millis(1),labelB,Color.web("#242424"),Color.web("#4486F4"));
+			 
+			 PathTransition path3 = new PathTransition();
+			 path3.setDuration(Duration.millis(150));
+			 path3.setPath(line3);
+			 path3.setNode(labelC);
+			 
+			 FillTransition  fillC = new FillTransition ( Duration.millis(1),labelC,Color.web("#242424"),Color.web("#FABB05"));
+			 
+			 PathTransition pathA = new PathTransition();
+			 pathA.setDuration(Duration.millis(30));
+			 pathA.setPath(lineA);
+			 pathA.setNode(labelA);
+			 
+			 PathTransition pathB = new PathTransition();
+			 pathB.setDuration(Duration.millis(30));
+			 pathB.setPath(lineB);
+			 pathB.setNode(labelB);
+			 
+			 
+			 PathTransition pathC = new PathTransition();
+			 pathC.setDuration(Duration.millis(30));
+			 pathC.setPath(lineC);
+			 pathC.setNode(labelC);
+			 
+			 
+			 
+			 
+		     EventHandler<ActionEvent> eventHandlerC = e ->{
+			     pane.getChildren().add(labelC); };
+			 
+			 EventHandler<ActionEvent> eventHandlerB = e ->{
+				 mediaPlayer.play();
+				 pane.getChildren().add(labelB);};
+				 
+				 EventHandler<ActionEvent> eventHandlerAll = e ->{					 
+					 pathA.play();
+					 pathB.play();
+					 pathC.play();
+					
+					};
+
+		 KeyFrame KeyFrameC = new KeyFrame(Duration.millis(1),eventHandlerC);
+		 KeyFrame KeyFrameB = new KeyFrame(Duration.millis(1),eventHandlerB);
+		 KeyFrame KeyFrameAll = new KeyFrame(Duration.millis(1),eventHandlerAll);
+
+			 
+	
+		 Timeline animB = new Timeline (KeyFrameB);
+		 Timeline animC = new Timeline(KeyFrameC);
+		 Timeline animAll = new Timeline(KeyFrameAll);
+			 
+			 pane.getChildren().add(labelA);
+			 
+			 SequentialTransition sTransition1 = new SequentialTransition();
+		 sTransition1.getChildren().addAll(path1,fillA,animB,parallel,fillB,animC,path3,fillC,animAll);
+	
+		 sTransition1.play();
+		 pane.getChildren().add(labelB);
+		 			 
+			 Scene scene = new Scene(pane,800,400);
+			 
+			 primaryStage.setTitle("FontDemo");
+		   primaryStage.setScene(scene);
+			 primaryStage.show();
+		}
+		public static void main(String[] args) {
+			launch(args);	
+		}
+			
+		}
+
+
